59dc021c66ec06d33d00360ce0bcb88e09cf4e11
karma-platform <social@karmaplatform.io>
2016-11-23 19:16:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'KARMA PLATFORM',
+  community: process.env.COMMUNITY_NAME || 'KARMA',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'karmaplatform.slack.com',
   // access token of slack
