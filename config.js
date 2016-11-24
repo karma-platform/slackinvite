a96c9188188b8c2b9db358c9173ece14bc7751f5
karma-platform <social@karmaplatform.io>
2016-11-24 11:43:31 +0530

    updated to original

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'KARMA',
+  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'karmaplatform.slack.com',
+  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
@@ -12,9 +12,10 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'https://hooks.slack.com/services/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',
+  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
 
   locale: process.env.LOCALE || "en",
 };
+
